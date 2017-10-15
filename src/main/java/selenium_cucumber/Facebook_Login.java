8778dc220fccadfccb87d629c51f6162c4d7325c
ramanamuttana <ramanamuttana@gmail.com>
2017-10-15 18:27:58 +0200

    Selenium and cucumber

--- src/main/java/selenium_cucumber/Facebook_Login.java
+++ src/main/java/selenium_cucumber/Facebook_Login.java
@@ -7,8 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Facebook_Login {
-{	
+	
 	WebDriver driver;
+	
 	@Given("^Open Firefox and open the Facebook page$")
 	public void Open_Firefox_and_open_the_Facebook_page1() throws Throwable {
 		System.setProperty("webdriver.gecko.driver", "E:\\Geckodriver\\geckodriver.exe");
@@ -16,22 +17,13 @@ public class Facebook_Login {
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
 	}
-	@Given("^Open Firefox and open the Facebook page$")
-	public void Open_Firefox_and_open_the_Facebook_page() throws Throwable 
-	{
-		
-	}
+	
 	@When("^I enter username and Password$")
 	public void I_enter_username_and_Password1() throws Throwable {
 		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("ramanamuttana@gmail.com");
-		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("Berlin14$");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("");
 	}
 	
-	@When("^I enter username and Password$")
-	public void I_enter_username_and_Password() throws Throwable 
-	{
-		
-	}
 	@Then("^I ahave to be on my home page$")
 	public void I_ahave_to_be_on_my_home_page() throws Throwable {
 		driver.findElement(By.id("loginbutton")).click();  
--- src/main/java/testRunners/TestRunner1.java
+++ src/main/java/testRunners/TestRunner1.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Features",glue={"cucumberselenium1"},format={"html:Cucumber/cucumber-html-report","json:Cucumber/cucumber-json-report"})
+@CucumberOptions(features="Features",glue={"selenium_cucumber"},format={"html:Cucumber/cucumber-html-report","json:Cucumber/cucumber-json-report"})
 
 public class TestRunner1 {
 
